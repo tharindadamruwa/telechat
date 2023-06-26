921d2be69f46186d022ebdb45ea2e9979f9d7fe7
tharindadamruwa <tharindadamruwan@gmail.com>
2023-06-26 07:48:22 +0530

    update config

--- php/config.php
+++ php/config.php
@@ -1,6 +1,7 @@
 <?php
 
-$conn = mysqli_connect("localhost", "root", "", "telechat");
+// $conn = mysqli_connect("localhost", "root", "", "telechat");
+$conn = mysqli_connect("sql203.infinityfree.com", "if0_34496732", "AL7AzzqhrO", "if0_34496732_telechat");
 
 if (!$conn) {
     echo "Database not connected" . mysqli_connect_error();
